c69ba5acdfed523f14ac34def15894c256f7d0b2
Felipe Lozano Romero <felipe.lozano1@alu.uclm.es>
2019-10-23 00:52:26 +0200

    Despliegue todo

--- demo/src/main/java/com/example/demo/controllers/PetsRestController.java
+++ demo/src/main/java/com/example/demo/controllers/PetsRestController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.entity.Pets;
 import com.example.demo.models.repository.IPetsRepository;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://pets-api-mongodb.herokuapp.com"})
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/api")
